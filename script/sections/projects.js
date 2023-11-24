3d78a999c1cf81e36caff08e8effba491d4d1b21
joão vitor <shepherdcom12@gmail.com>
2023-11-24 15:32:25 -0300

    add projects api

--- script/sections/projects.js
+++ script/sections/projects.js
@@ -1,32 +1,17 @@
-let projectsObj = [
-    {
-        projectName: "Lio Hairstyle",
-        projectMobileImg: "https://drive.google.com/file/d/1eKl8o17uEGhdtXvCPhJGku8jVIRxdqvZ/view?usp=drive_link",
-        projectDesktopImg: "assets/projects/lioHairstyle/desktop.jpeg"
-    },
-    {
-        projectName: "Marktec Estoque",
-        projectMobileImg: "https://drive.google.com/file/d/1eKl8o17uEGhdtXvCPhJGku8jVIRxdqvZ/view?usp=drive_link",
-        projectDesktopImg: "assets/projects/marktec/desktop.jpeg"
-    }, ,
-    {
-        projectName: "Lio Hairstyle",
-        projectMobileImg: "https://drive.google.com/file/d/1eKl8o17uEGhdtXvCPhJGku8jVIRxdqvZ/view?usp=drive_link",
-        projectDesktopImg: "https://drive.google.com/file/d/1lBmB9vgkHWVtjPlxEUwCbD3PSva7e6v3/view?usp=sharing"
-    },
-    {
-        projectName: "Lio Hairstyle",
-        projectMobileImg: "https://drive.google.com/file/d/1eKl8o17uEGhdtXvCPhJGku8jVIRxdqvZ/view?usp=drive_link",
-        projectDesktopImg: "https://drive.google.com/file/d/1lBmB9vgkHWVtjPlxEUwCbD3PSva7e6v3/view?usp=sharing"
-    }
-]
-
-let projectsDiv = document.getElementById("projectsDiv")
-projectsObj.forEach(element => {
-    let article = document.createElement("article")
-    projectsDiv.insertAdjacentElement("beforeend", article)
-    article.classList.add("projectCard")
-    article.innerHTML = `
+const apiUrl = "https://raw.githubusercontent.com/jvs-dev/projectsAPI/main/db.json";
+async function fetchData() {
+    try {
+        let response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar os dados. Código de status: ${response.status}`);
+        }
+        let projectsObj = await response.json();
+        let projectsDiv = document.getElementById("projectsDiv")
+        projectsObj.forEach(element => {
+            let article = document.createElement("article")
+            projectsDiv.insertAdjacentElement("beforeend", article)
+            article.classList.add("projectCard")
+            article.innerHTML = `
         <div class="projectCard__div--1">
             <img src="${element.projectDesktopImg}" alt="" class="projectCard__img">
         </div>
@@ -34,4 +19,10 @@ projectsObj.forEach(element => {
             <p class="projectCard__p">${element.projectName}</p>
             <button class="projectCard__btn">Ver mais</button>
         </div>`
-});
+        });
+    } catch (error) {
+
+    }
+}
+
+fetchData();
